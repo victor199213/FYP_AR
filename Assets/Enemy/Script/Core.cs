aec13ce5c113f81ed9d157e09ccfc84db3375557
TSITCL\s162133 <trident0310@gmail.com>
2017-10-03 13:54:39 +0900

    Pauseupdate

--- Assets/Enemy/Script/Core.cs
+++ Assets/Enemy/Script/Core.cs
@@ -44,8 +44,8 @@ public class Core : MonoBehaviour
         {
             Debug.Log("pause");
             SpawnManager.CancelInvoke();
-            PausableObjects.pausing = true;
             Instantiate(MenuMaker);
+            PausableObjects.pausing = true;
         }
     }
 }
