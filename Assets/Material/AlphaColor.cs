38b9a53019a7ccc76fc519d2207c4cf21d64117b
SEETOO WEI JIE VICTOR <150452J@sidm.student.local>
2017-10-06 15:06:17 +0800

    Art Asset in game scene

---
+++ Assets/Material/AlphaColor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlphaColor : MonoBehaviour
+{
+    private Color color;
+    private Renderer rend;
+
+    void Start ()
+    {
+        color = GetComponent<Renderer>().material.color;
+        color.a -= 0.25f;
+        rend = GetComponent<Renderer>();
+        rend.material.shader = Shader.Find("Transparent/Diffuse");
+        GetComponent<Renderer>().material.SetColor("_Color", color);
+    }
+
+    private void Update()
+    {
+        if (this.gameObject.tag == "Wall")
+        {
+            this.rend.enabled = false;
+        }
+        if (this.gameObject.tag == "coreObjective")
+        {
+            this.rend.enabled = false;
+        }
+    }
+}
