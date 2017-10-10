c2db007785185fe4b61c21755fe8af4fbfa54237
TSITCL\s162133 <trident0310@gmail.com>
2017-10-10 09:32:41 +0900

    pauseupdate

--- Assets/Pause/Menu.cs
+++ Assets/Pause/Menu.cs
@@ -8,14 +8,18 @@ public class Menu : MonoBehaviour {
     Pausable PausableObjects;
     SpawnManager SpawnManager;
     bool toggle;
-
+    Renderer rend;
+    MeshRenderer TitleButton;
+    MeshRenderer ResumeButton;
     // Use this for initialization
     void Start () {
         PausableObjects = GameObject.Find("PausableObjects").GetComponent<Pausable>();
         SpawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
         MenuMaker = GameObject.Find("Pause");
         toggle = true;
-
+        rend = GetComponent<Renderer>();
+        TitleButton = GameObject.Find("TitleCube").GetComponent<MeshRenderer>();
+        ResumeButton = GameObject.Find("ResumeCube").GetComponent<MeshRenderer>();
     }
     
     // Update is called once per frame
@@ -30,14 +34,16 @@ public class Menu : MonoBehaviour {
         if (col.gameObject.gameObject.tag == "coreObjective")
         {
             SpawnManager.CancelInvoke();
-            MenuMaker.transform.localScale = new Vector3(1, 1, 1);
+            TitleButton.enabled = true;
+            ResumeButton.enabled = true;
             PausableObjects.pausing = true;
             Time.timeScale = 0.1f;
             //Instantiate(MenuMaker);
         }
         if (col.gameObject.gameObject.tag == "Resume")
         {
-            MenuMaker.transform.localScale = new Vector3(0, 0, 0);
+            TitleButton.enabled = false;
+            ResumeButton.enabled = false;
             PausableObjects.pausing = false;
             Time.timeScale = 1.0f;
         }
