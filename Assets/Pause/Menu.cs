e308ff0d7019d3fe8ae32ed95dc6c9b3dbb14a9d
TSITCL\s162133 <trident0310@gmail.com>
2017-10-11 11:00:36 +0900

    ObjectUpdate

--- Assets/Pause/Menu.cs
+++ Assets/Pause/Menu.cs
@@ -11,6 +11,7 @@ public class Menu : MonoBehaviour {
     Renderer rend;
     MeshRenderer TitleButton;
     MeshRenderer ResumeButton;
+    MeshRenderer PauseUI;
     // Use this for initialization
     void Start () {
         PausableObjects = GameObject.Find("PausableObjects").GetComponent<Pausable>();
@@ -18,8 +19,9 @@ public class Menu : MonoBehaviour {
         MenuMaker = GameObject.Find("Pause");
         toggle = true;
         rend = GetComponent<Renderer>();
-        TitleButton = GameObject.Find("TitleCube").GetComponent<MeshRenderer>();
-        ResumeButton = GameObject.Find("ResumeCube").GetComponent<MeshRenderer>();
+        TitleButton = GameObject.Find("TitleButton").GetComponentInChildren<MeshRenderer>();
+        ResumeButton = GameObject.Find("ResumeButton").GetComponentInChildren<MeshRenderer>();
+        PauseUI = GameObject.Find("MenuBorder").GetComponentInChildren<MeshRenderer>();
     }
     
     // Update is called once per frame
@@ -36,7 +38,9 @@ public class Menu : MonoBehaviour {
             SpawnManager.CancelInvoke();
             TitleButton.enabled = true;
             ResumeButton.enabled = true;
+            PauseUI.enabled = true;
             PausableObjects.pausing = true;
+
             Time.timeScale = 0.1f;
             //Instantiate(MenuMaker);
         }
@@ -44,13 +48,14 @@ public class Menu : MonoBehaviour {
         {
             TitleButton.enabled = false;
             ResumeButton.enabled = false;
+            PauseUI.enabled = false;
+
             PausableObjects.pausing = false;
             Time.timeScale = 1.0f;
         }
         if (col.gameObject.gameObject.tag == "TitleButton")
         {
             GameObject.Find("Load").GetComponent<Load>().LoadNextScene("TitleScene/Title");
-         
         }
 
     }
