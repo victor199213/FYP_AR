c031e6e59a5fe0ccecb3b4cb95d8ebe069b2550f
Jagureo <jagnzet@gmail.com>
2017-10-30 10:50:23 +0800

    derendercylinder

---
+++ Assets/Towers/Script/DeRenderCylinder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeRenderCylinder : MonoBehaviour {
+
+    private int timer = 2;
+	// Use this for initialization
+	void Start () {
+		GetComponent<MeshRenderer>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(timer > 0)
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+            timer--;
+        }
+    }
+}
